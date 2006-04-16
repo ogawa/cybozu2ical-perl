365c982b83fe0f95677ece335b6a3befa04af4c1
Hirotaka Ogawa <hirotaka.ogawa@gmail.com>
2006-04-16 10:32:22 +0000

    Add more robust timezone support. Add a document.
    git-svn-id: file:///Users/ogawa/svn/cybozu2ical/trunk@5 b7bee0e5-6241-4529-aa9e-08ae86e46850

--- cybozu2ical.pl
+++ cybozu2ical.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
-# cybozu2ical.pl: converting CybozuOffice6 calendar into iCal format
+# cybozu2ical: Convert Cybozu Office 6 calendar into iCal format
 #
-# Hirotaka Ogawa (hirotaka.ogawa at gmail.com)
+# $Id$
 
 use strict;
 use YAML;
@@ -12,11 +12,13 @@ use Data::ICal;
 use Data::ICal::Entry::Event;
 use Data::ICal::Entry::TimeZone;
 use Data::ICal::Entry::TimeZone::Standard;
-use Date::ICal;
+use DateTime;
+use DateTime::TimeZone;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 my $cfg = YAML::LoadFile($ARGV[0] || 'config.yaml');
+my $tz = DateTime::TimeZone->new(name => $cfg->{time_zone} || 'Asia/Tokyo');
 
 my $ua = LWP::UserAgent->new();
 
@@ -47,7 +49,7 @@ $vcalendar->add_properties(
 );
 
 # current timestamp
-my $dtstamp = Date::ICal->new(epoch => time)->ical;
+my $dtstamp = dt2ical(DateTime->from_epoch(epoch => time));
 
 while ($#lines != -1) {
     my $line = shift(@lines);
@@ -58,26 +60,15 @@ while ($#lines != -1) {
 
     my $epoch = $fields[1];
     $epoch =~ s/ts\.//;
-    my $created = Date::ICal->new(epoch => $epoch)->ical;
+    my $created = dt2ical(DateTime->from_epoch(epoch => $epoch));
 
-    my @d = split("/", $fields[3]);
-    my @t = split(":", $fields[5]);
-    my $date = Date::ICal->new(
-	year => $d[0], month => $d[1], day => $d[2],
-	hour => $t[0] || 0, min => $t[1] || 0, sec => $t[2] || 0
-    );
-    my $dtstart = $date->ical(offset => '+0900');
-    $dtstart =~ s/T000000// if $fields[5] =~ /^:$/;
+    my $dt1 = cydate2dt($fields[3], $fields[5], $tz);
+    my $dtstart = ($fields[5] !~ /^:$/) ?
+	dt2ical($dt1) : $dt1->ymd('');
 
-    my @d = split("/", $fields[4]);
-    my @t = split(":", $fields[6]);
-    my $date = Date::ICal->new(
-	year => $d[0], month => $d[1], day => $d[2],
-	hour => $t[0] || 0, min => $t[1] || 0, sec => $t[2] || 0
-    );
-    $date += 'P1D' if $fields[6] =~ /^:$/; # full-day or multiple-days events
-    my $dtend = $date->ical(offset => '+0900');
-    $dtend =~ s/T000000// if $fields[6] =~ /^:$/;
+    my $dt2 = cydate2dt($fields[4], $fields[6], $tz);
+    my $dtend = ($fields[6] !~ /^:$/) ?
+	dt2ical($dt2) : $dt2->add(days => 1)->ymd('');
 
     my $vevent = Data::ICal::Entry::Event->new();
     $vevent->add_properties(
@@ -93,19 +84,139 @@ while ($#lines != -1) {
 }
 
 my $vtimezone = Data::ICal::Entry::TimeZone->new();
-$vtimezone->add_properties(tzid => 'Asia/Tokyo');
+$vtimezone->add_properties(tzid => $tz->name);
 
+# probably we need to support the Daylight Saving Time
 my $standard = Data::ICal::Entry::TimeZone::Standard->new();
-$standard->add_properties(
-    tzoffsetfrom => '+0900',
-    tzoffsetto => '+0900',
-    tzname => 'JST',
-    dtstart => '19700101T000000'
-);
 
+my $dt = cydate2dt("1970/01/01", "00:00:00", $tz);
+my $offset = DateTime::TimeZone::offset_as_string($dt->offset) || '+0900';
+my $tzname = $cfg->{tzname} || $tz->short_name_for_datetime($dt) || 'JST';
+
+$standard->add_properties(
+    tzoffsetfrom => $offset,
+    tzoffsetto => $offset,
+    tzname => $tzname,
+    dtstart => dt2ical($dt)
+);
 $vtimezone->add_entry($standard);
+
 $vcalendar->add_entry($vtimezone);
 
 print $vcalendar->as_string;
 
+sub cydate2dt {
+    my($date, $time, $tz) = @_;
+    my @d = split("/", $date);
+    my @t = split(":", $time);
+
+    my $dt = DateTime->new(
+	year => $d[0], month => $d[1], day => $d[2],
+	hour => $t[0] || 0, minute => $t[1] || 0, second => $t[2] || 0,
+	time_zone => $tz || 'Asia/Tokyo'
+    );
+    return $dt;
+}
+
+sub dt2ical {
+    my($dt) = @_;
+    $dt->ymd('') . 'T' . $dt->hms('') . ($dt->time_zone->is_utc ? 'Z' : '');
+}
+
 1;
+=head1 NAME
+
+cybozu2ical - Convert CybozuOffice6 calendar into iCal format
+
+=head1 SYNOPSIS
+
+  % cybozu2ical
+  % cybozu2ical /path/to/config.yaml
+
+=head1 DESCRIPTION
+
+=head1 DESCRIPTION
+
+C<cybozu2ical> is a command line application that fetches Cybozu
+Office 6 calendar items and converts them into a iCal file.  It allows
+you to easily integrate the Cybozu Calendar into iCalendar-enabled
+Calendar applications, such as Microsoft Outlook, Apple iCal, and of
+course, Google Calendar.
+
+You can run this via crontab, for example, every 1 hour.
+
+=head1 REQUIREMENT
+
+This application requires perl 5.8.0 with following Perl modules
+installed on your box.
+
+=over 4
+
+=item Data::ICal
+
+=item DateTime
+
+=item YAML
+
+=back
+
+=head1 OPTIONS
+
+This application has a command-line option as follows:
+
+=over 4
+
+=item path/to/config.yaml
+
+Specified the path to a configuration file. By default, C<config.yaml>
+in the current directory.
+
+=back
+
+=head1 CONFIGURATION
+
+The distributions includes a sample configuration file
+C<config.yaml.sample>. You can rename it to C<config.yaml> and
+configure C<cybozu2ical>.
+
+=over 4
+
+=item cybozu_url
+
+Set the URL of your Cybozu Office 6.
+
+=item username, password
+
+Set your username and password for Cybozu Office 6.
+
+=item time_zone
+
+Set the timezone of your Cybozu Office 6 (e.g., Asia/Tokyo).
+
+=item tzname
+
+Set the short timezone name of your Cybozu Office 6 (e.g., JST).
+
+=back
+
+=head1 DEVELOPMENT
+
+The development version is always available from the following
+subversion repository:
+
+  svn://svn.as-is.net/public/cybozu2ical/trunk
+
+You can browse the files via SVN::Web from the following:
+
+  http://svn.as-is.net/svnweb/public/browse/cybozu2ical/trunk/
+
+Any comments, suggestions, or patches are welcome.
+
+=head1 AUTHOR
+
+Hirotaka Ogawa E<lt>hirotaka.ogawa at gmail.comE<gt>
+
+This script is free software and licensed under the same terms as Perl
+(Artistic/GPL).
+
+=cut
